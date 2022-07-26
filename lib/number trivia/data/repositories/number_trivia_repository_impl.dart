1ba74f7f03e5e687571ed445d88ad8e3ed858f6e
Ali ramadan <aramadan@ipmagix.com>
2022-07-26 16:00:36 +0200

    Initial commit

---
+++ lib/number trivia/data/repositories/number_trivia_repository_impl.dart
@@ -0,0 +1,63 @@
+import 'package:dartz/dartz.dart';
+import 'package:number_trivia/core/error/exception.dart';
+import 'package:number_trivia/core/error/failures.dart';
+import 'package:number_trivia/core/network/network_info.dart';
+import 'package:number_trivia/number%20trivia/data/data_sources/number_trivia_local_data_source.dart';
+import 'package:number_trivia/number%20trivia/data/data_sources/number_trivia_remote_data_source.dart';
+import 'package:number_trivia/number%20trivia/domain/entities/number_trivia.dart';
+import 'package:number_trivia/number%20trivia/domain/repositories/number_trivia_repository.dart';
+
+class NumberTriviaRepositoryImpl implements NumberTriviaRepository {
+  final NumberTriviaRemoteDataSource remoteDataSource;
+  final NumberTriviaLocalDataSource localDataSource;
+  final NetworkInfo networkInfo;
+
+  NumberTriviaRepositoryImpl({
+    required this.remoteDataSource,
+    required this.localDataSource,
+    required this.networkInfo,
+  });
+  @override
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(
+      int number) async {
+    // Finally doing something with the value of isConnected 😉
+    if (await networkInfo.isConnected) {
+      try {
+        final remoteTrivia =
+            await remoteDataSource.getConcreteNumberTrivia(number);
+        localDataSource.cacheNumberTrivia(remoteTrivia);
+        return Right(remoteTrivia);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      try {
+        final localTrivia = await localDataSource.getLastNumberTrivia();
+        return Right(localTrivia);
+      } on CacheException {
+        return Left(CacheFailure());
+      }
+    }
+  }
+
+  @override
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia() async {
+    // Finally doing something with the value of isConnected 😉
+    if (await networkInfo.isConnected) {
+      try {
+        final remoteTrivia = await remoteDataSource.getRandomNumberTrivia();
+        localDataSource.cacheNumberTrivia(remoteTrivia);
+        return Right(remoteTrivia);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      try {
+        final localTrivia = await localDataSource.getLastNumberTrivia();
+        return Right(localTrivia);
+      } on CacheException {
+        return Left(CacheFailure());
+      }
+    }
+  }
+}
---
+++ lib/number trivia/presentation/bloc/number_trivia_bloc.dart
@@ -0,0 +1,83 @@
+import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:number_trivia/core/error/failures.dart';
+import 'package:number_trivia/core/usecase/usecase.dart';
+import 'package:number_trivia/core/util/input_converter.dart';
+import 'package:number_trivia/number%20trivia/domain/entities/number_trivia.dart';
+import 'package:number_trivia/number%20trivia/domain/use_cases/get_concrete_number_trivia.dart';
+import 'package:number_trivia/number%20trivia/domain/use_cases/get_random_number_trivia.dart';
+import 'number_trivia_state.dart';
+import 'number_trivia_event.dart';
+
+const String SERVER_FAILURE_MESSAGE = 'Server Failure';
+const String CACHE_FAILURE_MESSAGE = 'Cache Failure';
+const String INVALID_INPUT_FAILURE_MESSAGE =
+    'Invalid Input - The number must be a positive integer or zero.';
+
+class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
+  final GetConcreteNumberTrivia getConcreteNumberTrivia;
+  final GetRandomNumberTrivia getRandomNumberTrivia;
+  final InputConverter inputConverter;
+
+  NumberTriviaBloc({
+    // Changed the name of the constructor parameter (cannot use 'this.')
+    required GetConcreteNumberTrivia concrete,
+    required GetRandomNumberTrivia random,
+    required this.inputConverter,
+  })  : assert(concrete != null),
+        assert(random != null),
+        assert(inputConverter != null),
+        getConcreteNumberTrivia = concrete,
+        getRandomNumberTrivia = random;
+
+  @override
+  NumberTriviaState get initialState => Empty();
+  @override
+  Stream<NumberTriviaState> mapEventToState(
+    NumberTriviaEvent event,
+  ) async* {
+    if (event is GetTriviaForConcreteNumber) {
+      final inputEither =
+          inputConverter.stringToUnsignedInteger(event.numberString);
+
+      yield* inputEither.fold(
+        (failure) async* {
+          yield Error(message: INVALID_INPUT_FAILURE_MESSAGE);
+        },
+        (integer) async* {
+          yield Loading();
+          final failureOrTrivia = await getConcreteNumberTrivia(
+            Params(number: integer),
+          );
+          yield* _eitherLoadedOrErrorState(failureOrTrivia);
+        },
+      );
+    } else if (event is GetTriviaForRandomNumber) {
+      yield Loading();
+      final failureOrTrivia = await getRandomNumberTrivia(
+        NoParams(),
+      );
+      yield* _eitherLoadedOrErrorState(failureOrTrivia);
+    }
+  }
+
+  Stream<NumberTriviaState> _eitherLoadedOrErrorState(
+    Either<Failure, NumberTrivia> either,
+  ) async* {
+    yield either.fold(
+      (failure) => Error(message: _mapFailureToMessage(failure)),
+      (trivia) => Loaded(trivia: trivia),
+    );
+  }
+
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return SERVER_FAILURE_MESSAGE;
+      case CacheFailure:
+        return CACHE_FAILURE_MESSAGE;
+      default:
+        return 'Unexpected Error';
+    }
+  }
+}
